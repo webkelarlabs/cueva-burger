edb69f2c5b68a794f2ceb78874737eba2cc63345
Ezequiel <ezequielezequiel9@gmail.com>
2025-09-04 18:50:33 -0300

    edit server route

--- backend/server.ts
+++ backend/server.ts
@@ -56,13 +56,13 @@ app.post("/api/chat", async (req, res) => {
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 // });
-const frontendPath = path.resolve(__dirname, "../../frontend/dist"); // ajusta según tu build
+const frontendPath = path.resolve(__dirname, "../../frontend/dist");
+
 app.use(express.static(frontendPath));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
